8a86a72725a909292dd2d9bc9dbc0b966ba6d1f1
gabriel@kira <92814110+GabrielAugustoBer@users.noreply.github.com>
2024-03-07 10:47:35 -0300

    add components

--- src/components/Ingredient/index.tsx
+++ src/components/Ingredient/index.tsx
@@ -1,13 +1,26 @@
-import { Pressable, Text, Image } from "react-native";
-import { styles } from "./styles";
-import React from "react";
+import { Text, Pressable, PressableProps, Image } from "react-native"
 
-export function Ingredient(){
-    return (
-        <Pressable style={styles.container}>
-            <Image style={styles.image} />
+import { styles } from "./styles"
 
-            <Text style={styles.title}>Maça</Text>
-        </Pressable>
-    )
+export type IngredientsProps = {
+  name: string
+  image: string
+  selected?: boolean
+}
+
+export function Ingredient({
+  name,
+  image,
+  selected = false,
+  ...rest
+}: IngredientsProps & PressableProps) {
+  return (
+    <Pressable
+      style={[styles.container, selected && styles.selected]}
+      {...rest}
+    >
+      <Image source={{ uri: image }} style={styles.image} />
+      <Text style={styles.title}>{name}</Text>
+    </Pressable>
+  )
 }
